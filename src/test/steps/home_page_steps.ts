90fde7177c6863c5880e45c6646e40225dbfd653
Amlana Kumar Sahoo <Amlana.Sahoo@dwyergroup.com>
2026-01-17 17:36:15 +0530

    fix

--- src/test/steps/home_page_steps.ts
+++ src/test/steps/home_page_steps.ts
@@ -1,29 +1,3 @@
-// import { HomePageOperations } from '@src/main/operations/HomePageOperations';
-// import { getAutoExeApp } from '@src/main/utilities/autoExe-utils';
-// import { expect } from '@playwright/test';
-// import { Given, When, Then } from "@cucumber/cucumber";
-// export let homePage:HomePageOperations;
-// let actualResult:string|null;
-// Given('User Visits HomePage', async ({page}) => {
-//   homePage = await getAutoExeApp(page) as HomePageOperations;
-// });
-
-// When('User Observes HomePage Title', async ({}) => {
-//   actualResult =await homePage.getTitle();
-// });
-
-// Then('title should match {string}', async ({}, arg) => {
-//   expect(actualResult).toEqual(arg);
-// });
-
-// When('User Observes sub Title', async ({}) => {
-//   actualResult =await homePage.getSubTitle();
-// });
-
-// Then('sub title should match {string}', async ({}, arg: string) => {
-//   expect(actualResult).toEqual(arg);
-// });
-
 import { HomePageOperations } from '../../main/operations/HomePageOperations';
 import { getAutoExeApp } from '../../main/utilities/autoExe-utils';
 import { expect, Page } from '@playwright/test';
--- src/test/steps/productsPage_steps.ts
+++ src/test/steps/productsPage_steps.ts
@@ -1,39 +1,3 @@
-// import { Given, When, Then } from "@cucumber/cucumber";
-// import { ProductsPageOperations } from "@src/main/operations/ProductsPageOperations";
-// import { getProductsPageApp } from "@src/main/utilities/autoExe-utils";
-// import { expect, Page } from "@playwright/test";
-
-// export let productsPage: ProductsPageOperations;
-
-// When('User navigates to Products tab', async ({page}) => {
-//     productsPage = await getProductsPageApp(page) as ProductsPageOperations;
-//     await productsPage.navigateToProductsPage();
-
-// });
-
-// Then('User should be redirected to All Products page', async ({}) => {
-//     expect(await productsPage.verifyAllProductsPage()).toBe('All Products');
-
-// });
-
-// Then('User verifies product list', async ({}) => {
-//     expect(await productsPage.verifyProductListCount()).toBeGreaterThan(0);
-   
-// });
-
-// Then('User clicks on first product', async ({}) => {
-//     await productsPage.viewFirstProduct();
-// });
-
-// Then('User landed to product detail page', async ({}) => {
-//     await productsPage.verifyProductDetails();
-//     expect(await productsPage.verifyProductDetails()).toBeTruthy();
-// });
-
-// Then('User verifies product name, category, price, availability, condition, brand', async ({}) => {
-//     expect(await productsPage.verifyProductDetails()).toBeTruthy();
-// });
-
 import { Given, When, Then } from '@cucumber/cucumber';
 import { expect, Page } from '@playwright/test';
 import { ProductsPageOperations } from '../../main/operations/ProductsPageOperations';
